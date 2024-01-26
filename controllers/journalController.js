35215995ac86e4ffc3ca4ff1ff4e5e09e7245208
Jalen <jalenms2012@gmail.com>
2024-01-26 11:29:27 -0800

    index and delete routes added

--- controllers/journalController.js
+++ controllers/journalController.js
@@ -1,1 +1,19 @@
 const Journal = require('../models/journal')
+
+//Index
+exports.indexJournal = async (req, res) => {
+    try {
+        const foundJournals = await Journal.find({})
+        res.status(200).json({ journals: foundJournals})
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+//Delete
+
+//Update
+
+//Create
+
+//Show
